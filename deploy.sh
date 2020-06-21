3b2a63de833de2b18f93088018eaad19405a351d
K <kamiyaowl@gmail.com>
2020-06-22 00:22:59 +0900

    ref #26 dotnet testのxml出力をhtml reportとして出力するよう修正

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,17 @@
 dotnet publish -c Release
 
 # run and report xml file
-dotnet test --collect:"XPlat Code Coverage"
-
+dotnet test --collect:"XPlat Code Coverage" --logger "junit;LogFileName=unittest.result.xml"
 # copy ./BlazeSnes.Core.Test/TestResults/{hash}/coverage.cobertura.xml
 find ./BlazeSnes.Core.Test/TestResults/ | tail -n 1 | xargs -I "{}" cp "{}" ./
-
-# create report to ./coverage
+# create code coverage report to ./coverage
 reportgenerator "-reports:coverage.cobertura.xml" "-targetdir:coverage" "-reporttypes:Html;Badges"
-
 # copy coverage reports to publish directory
 cp -r ./coverage ./BlazeSnes/bin/Release/netstandard2.1/publish/wwwroot/
+
+# crate unit test report
+mkdir -p ./unittest
+junit2html BlazeSnes.test/unittest.result.xml ./unittest/blazesnes.html
+junit2html BlazeSnes.Core.test/unittest.result.xml ./unittest/blazesnes.core.html
+# copy unit test reports to publish directory
+cp -r ./unittest ./BlazeSnes/bin/Release/netstandard2.1/publish/wwwroot/
