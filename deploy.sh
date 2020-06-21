92f70a145c447695ca17beaf3b6b9cf78b9bf086
K <kamiyaowl@gmail.com>
2020-06-22 01:07:45 +0900

    ref #30 TestResults.xmlはデフォルトパスのまま利用する

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 dotnet publish -c Release
 
 # run and report xml file
-dotnet test --collect:"XPlat Code Coverage" --logger "junit;LogFileName=unittest.result.xml"
+dotnet test --collect:"XPlat Code Coverage" --logger "junit;"
 # copy ./BlazeSnes.Core.Test/TestResults/{hash}/coverage.cobertura.xml
 find ./BlazeSnes.Core.Test/TestResults/ | tail -n 1 | xargs -I "{}" cp "{}" ./
 # create code coverage report to ./coverage
@@ -13,7 +13,7 @@ cp -r ./coverage ./BlazeSnes/bin/Release/netstandard2.1/publish/wwwroot/
 
 # crate unit test report
 mkdir -p ./unittest
-junit2html BlazeSnes.test/unittest.result.xml ./unittest/blazesnes.html
-junit2html BlazeSnes.Core.test/unittest.result.xml ./unittest/blazesnes.core.html
+junit2html BlazeSnes.Test/TestResults/TestResults.xml ./unittest/blazesnes.html
+junit2html BlazeSnes.Core.Test/TestResults/TestResults.xml ./unittest/blazesnes.core.html
 # copy unit test reports to publish directory
 cp -r ./unittest ./BlazeSnes/bin/Release/netstandard2.1/publish/wwwroot/
