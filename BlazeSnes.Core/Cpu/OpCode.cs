6e0f28584a6dd2e16cfe7fb2188354bb719affe3
K <kamiyaowl@gmail.com>
2020-06-22 01:58:02 +0900

    ref #22 OpCode対応の定義を追加

--- BlazeSnes.Core/Cpu/OpCode.cs
+++ BlazeSnes.Core/Cpu/OpCode.cs
@@ -1,6 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace BlazeSnes.Core.Cpu {
-    // TODO: OpCode -> Instruction, Addressing対応を作成
+    public static class OpCode {
+        /// <summary>
+        /// OpCodeと命令の対応を示します
+        /// </summary>
+        public static readonly SortedDictionary < byte, (Instruction, Addressing) > OpCodes = new SortedDictionary < byte, (Instruction, Addressing) > () {
+            // { 0x61, ( Instruction.ADC, Addressing.indexedin ) }, // TODO: Addressing Modeを一通りちゃんと見る
+            { 0x63, (Instruction.ADC, Addressing.StackRelative) },
+            // TODO: 全部作る
+        };
+    }
 }
