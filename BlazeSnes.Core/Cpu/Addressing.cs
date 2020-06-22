7aa0210ff495c17d3545652988dd5bc5ab2d5a91
K <kamiyaowl@gmail.com>
2020-06-23 02:16:50 +0900

    ref #22 AddressingModeの定義を修正

--- BlazeSnes.Core/Cpu/Addressing.cs
+++ BlazeSnes.Core/Cpu/Addressing.cs
@@ -6,28 +6,29 @@ namespace BlazeSnes.Core.Cpu {
     /// アドレッシングモード一覧
     /// </summary>
     public enum Addressing {       
-        Implied,
-        ImmediateMemoryFlag,
-        ImmediateIndexFlag,
-        Immediate,
-        Relative,
-        RelativeLong,
-        Direct,
-        DirectIndexedX,
-        DirectIndexedY,
-        DirectIndexedIndirectX,
-        DirectIndirectIndexedLong,
-        Absolute,
-        AbsoluteX,
-        AbsoluteY,
-        AbsoluteLong,
-        AbsoluteIndexedLong,
-        StackRelative,
-        StackRelativeIndirectIndexed,
-        AbsoluteIndirect,
-        AbsoluteIndirectLong,
-        AbsoluteIndexedIndirect,
-        ImpliedAccumulator,
-        BlockMove,
+        Implied, // 指定なし
+        Accumulator, // (A)の値
+        Immediate, // (IM8)の値 IM8
+        Direct,// *(IM8)の値 IM8
+        DirectIndexedX, // *(IM+X)の値 IM8, X
+        DirectIndexedY, // *(IM+Y)の値 IM8, Y
+        Relative, // *(PC+IM8)の値 IM8
+        Absolute,// *(DB * $1_0000 + IM16)の値 IM16
+        AbsoluteIndexedX, // *(DB * $1_0000 + IM16 + X)の値 IM16, X
+        AbsoluteIndexedY, // *(DB * $1_0000 + IM16 + Y)の値 IM16, Y
+        AbsoluteLong, // *(IM24)の値 IM24
+        AbsoluteLongIndexedX, // *(IM24+X)の値 IM24, X
+        DirectIndexedIndirectX, // *(DB * $1_0000 + *(IM8+X))の値 IM8, X
+        DirectIndirectIndexedY, // *(DB * $1_0000 + *IM8 + Y)の値 (IM8), Y
+        DirectIndirectLong, // *(*(IM8))の値 [IM8]
+        DirectIndirectLongIndexed, // *(*(IM8) + Y)の値 [IM8]
+        AbsoluteIndirect, // *(IM16)の値 (IM16)
+        AbsoluteIndexedIndirectX, // *(IM16 + X)の値 (IM16, X)
+        AbsoluteIndirectLong, // *(IM24)の値 [IM24]
+        // ProgramCounterRelative, // *(PC+IM16)の値
+        // ProgramCounterRelativeLong, // *(PC+IM24)の値
+        StackRelative, // 0: 最後にpushされた値, 1: 最後にpopされた値 $00,s or $01,s
+        StackRelativeIndirectIndexedY, // *(*(s) + Y) の値、StackRelativeから相対間接参照を実装したもの
+        BlockMove, // for MVN/MVP
     }
 }
