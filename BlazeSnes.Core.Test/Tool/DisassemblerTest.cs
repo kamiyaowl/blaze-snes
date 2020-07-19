3d2f23cf63944e6b067482cbdc0aad5e2965889c
K <kamiyaowl@gmail.com>
2020-07-19 16:35:53 +0900

    refs #33 SampleRomテストリファクタリング前

--- BlazeSnes.Core.Test/Tool/DisassemblerTest.cs
+++ BlazeSnes.Core.Test/Tool/DisassemblerTest.cs
@@ -16,6 +16,9 @@ namespace BlazeSnes.Core.Test.Tool {
     public class DisassemblerTest {
         static readonly string SAMPLE_PATH = @"../../../../assets/roms/helloworld/sample1.smc";
 
+        /// <summary>
+        /// Hello, Worldを出力するサンプルROMのDisassemblerが正しいことを検証します
+        /// </summary>
         [Fact]
         public void SampleRom() {
             Cartridge cartridge;
@@ -29,13 +32,14 @@ namespace BlazeSnes.Core.Test.Tool {
             }
             // LoROM ResetVector:00_a20eは 00-3f: 8000-ffffにマップされるので
             // a20e-8000=220eにマップされるはず
+            // 2200~220dには"HELLO, WORLD!\0" が格納されている
             var expectRomAddr = (cartridge.ResetAddrInEmulation - 0x8000);
             var (targetDevice, localAddr) = cartridge.ConvertToLocalAddr(cartridge.ResetAddrInEmulation);
             Assert.Equal(0x220e, expectRomAddr);
             Assert.Equal(0x220e, (int)localAddr);
             Assert.Equal(Cartridge.TargetDevice.Rom, targetDevice);
             // とりあえずデータが入っていそうな領域だけ展開してみる
-            var src = cartridge.RomData.Skip((int)localAddr).Take(10);
+            var src = cartridge.RomData.Skip((int)localAddr).Take(18);
             var dst = Disassembler.Parse(src).ToArray();
 
             // sample1.asm参照
@@ -49,9 +53,17 @@ namespace BlazeSnes.Core.Test.Tool {
             Assert.Equal(0x30, dst[5].Item2[0]); // args
             // .a16
             // .i16
-            Assert.Equal(Instruction.LDX, dst[6].Item1.Inst); // Immediate #1fff
+            Assert.Equal(Instruction.LDX, dst[6].Item1.Inst); // #1fff
             Assert.Equal(0xff, dst[6].Item2[0]); // args
             Assert.Equal(0x1f, dst[6].Item2[1]); // args
+            Assert.Equal(Instruction.TXS, dst[7].Item1.Inst); // Implied
+            Assert.Equal(Instruction.JSR, dst[8].Item1.Inst); // $InitRegs
+            Assert.Equal(0x82, dst[8].Item2[0]); // args
+            Assert.Equal(0xa2, dst[8].Item2[1]); // args
+            Assert.Equal(Instruction.SEP, dst[9].Item1.Inst); // #20
+            Assert.Equal(0x20, dst[9].Item2[0]); // args
+            // .a8
+
             // TODO: 期待ケースをもう少し伸ばす
         }
     }
