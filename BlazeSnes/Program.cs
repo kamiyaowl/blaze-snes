500f09649f155ea47e633442805efc6744ce44bb
K <kamiyaowl@gmail.com>
2020-07-24 02:56:24 +0900

    refs #96 Blazoriseを導入、最低限のUIを復元

--- BlazeSnes/Program.cs
+++ BlazeSnes/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 using Blazor.FileReader;
 
+using Blazorise;
+using Blazorise.Frolic;
+using Blazorise.Icons.FontAwesome;
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +19,13 @@ namespace BlazeSnes {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services
+                .AddBlazorise( options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                } )
+                .AddFrolicProviders()
+                .AddFontAwesomeIcons();
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
