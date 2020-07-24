656974665d8ce8fd3556128659241005739ae11a
K <kamiyaowl@gmail.com>
2020-07-25 02:55:41 +0900

    refs #24 Canvasの拡大に対応

--- BlazeSnes/wwwroot/js/index.razor.js
+++ BlazeSnes/wwwroot/js/index.razor.js
@@ -1,10 +1,20 @@
+const baseWidth = 256;
+const baseHeight = 224;
+let canvas;
+let context;
+let imageData;
+
+window.initEmulatorCanvas = function(e) {
+    canvas = document.getElementById('emulatorCanvas');
+    context = canvas.getContext('2d');
+    imageData = context.createImageData(baseWidth, baseHeight);
+}
 window.drawEmulatorCanvas = function(dataPtr) {
     // console.log('drawEmulatorCanvas', dataPtr);
-    var canvas = document.getElementById('emulatorCanvas');
-    var context = canvas.getContext('2d');
-    var imageData = context.createImageData(256, 224);
-
+    // convert from C# data
     imageData.data.set(Uint8ClampedArray.from(Module.HEAPU8.subarray(dataPtr, dataPtr + imageData.data.length)));
     context.putImageData(imageData, 0, 0);
-    context.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+    // draw
+    context.drawImage(canvas, 0, 0, baseWidth, baseHeight, 0, 0, canvas.width, canvas.height);
+    console.log(canvas.width, canvas.height);
 };
